19a4c273015b2e51be50425075aa0b8c086f717b
Lorenzo Laneve <lorenzo.laneve@usi.ch>
2025-01-31 15:06:19 +0100

    Add docs to utility functions

--- util.py
+++ util.py
@@ -2,31 +2,32 @@
 
 import mpmath as mp
 
-# easy computation of absolute value squared
 def abs2(z):
+    """Returns the absolute value squared."""
     return mp.re(z)**2 + mp.im(z)**2
 
-# Plots the given function(s) on the unit circle.
-# More explicitly, given f(z), f(e^ix) will be plotted with respect to x.
 def plot_on_circle(l):
+    """Plots the given functions on the unit circle. The functions must be given as complex functions :math:`f(z)`,
+    and the plot will be of :math:`f(e^ix)` for `x \in [-\pi, \pi]`.
+
+    Args:
+        l (function | list[functions]): The complex function(s) to be plotted.
+    """
     if not isinstance(l, list):
         l = [l]
 
     mp.plot([lambda x, f=f: f(mp.exp(1j * x)) for f in l], [-mp.pi, mp.pi])
 
-# Returns the number of decimal digits required to encode ceil(x)
-# This is used when x appears in the error bound, and the machine precision
-# has to be raised in order to suppress this term.
-def ceil_dps(x):
-    return int(mp.ceil(mp.log10(1 + x)))
+def coeffs_pad(c: list, N: int):
+    """Pads the list c with zeros so that it results of length N. If len(c) >= N, then the list will be left unchanged.
 
-# Returns the number of bits required to encode ceil(x)
-def ceil_bits(x):
-    return int(mp.ceil(mp.log(1 + x, b=2)))
+    Args:
+        c (list[complex]): the list to be padded
+        N (int): The length of the padded list.
 
-# Pads the list c so that it results of length N.
-# if len(c) >= N, then this function does nothing.
-def coeffs_pad(c, N):
+    Returns:
+        list[complex]: The original list padded with zeros, such that the total length will be N.
+    """
     if len(c) < N:
         return c + [mp.mpc(0)] * (N - len(c))
 
