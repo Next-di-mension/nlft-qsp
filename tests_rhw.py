34c7f8668039c8610d16854ef3d9dcc223c341da
Lorenzo Laneve <lorenzo.laneve@usi.ch>
2025-02-03 10:36:50 +0100

    Add Riemann-Hilbert factorization algorithm + tests

--- tests_rhw.py
+++ tests_rhw.py
@@ -3,6 +3,7 @@ import unittest
 
 import mpmath as mp
 
+import riemann_hilbert
 import weiss
 
 from mpm_fft import fft, ifft
@@ -33,7 +34,7 @@ class RHWTestCase(unittest.TestCase):
         self.assertAlmostEqual(max([abs(x - y) for x, y in zip(ifft(fft(seq)), seq)]), 0, delta=10**(-mp.mp.dps+4))
 
     @mp.workdps(30)
-    def test_weiss(self):
+    def test_rhw(self):
         b = random_polynomial(256, eta=0.7)
         a, c = weiss.ratio(b)
         
@@ -41,6 +42,20 @@ class RHWTestCase(unittest.TestCase):
 
         self.assertAlmostEqual(max([abs(c(mp.expjpi(2 * k/512)) - b(mp.expjpi(2 * k/512))/a(mp.expjpi(2 * k/512))) for k in range(512)]), 0, delta=10**(-mp.mp.dps+2))
         self.assertEqual(c.support().stop, b.support().stop)
+
+        Ap, Bp = riemann_hilbert.factorize(c, 10, normalize=True)
+        self.assertAlmostEqual((Ap * Ap.conjugate() + Bp * Bp.conjugate() - 1).l2_norm(), 0, delta=10**(-mp.mp.dps+2))
+
+    @mp.workdps(30)
+    def test_inlft_rhw(self):
+        b = random_polynomial(4, eta=0.5)
+        a, c = weiss.ratio(b)
+
+        nlft = riemann_hilbert.inlft(b, c)
+        a2, b2 = nlft.transform()
+
+        self.assertAlmostEqual((a - a2).l2_norm(), 0, delta=10**(-mp.mp.dps+2))
+        self.assertAlmostEqual((b - b2).l2_norm(), 0, delta=10**(-mp.mp.dps+2))
 
 
 if __name__ == '__main__':
