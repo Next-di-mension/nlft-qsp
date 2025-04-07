05f494254ab763e1f814fb8f38cb7ac8833364cc
Lorenzo Laneve <lorenzo.laneve@usi.ch>
2025-04-07 15:52:18 +0200

    Add bivariate support plot example

--- nlft_qsp/plot.py
+++ nlft_qsp/plot.py
@@ -15,7 +15,6 @@ def plot_support_2d(l: list, rng: tuple[range]):
     if len(rng) != 2:
         raise ValueError("rng must have dimension 2.")
 
-    plt.figure(figsize=(6, 6))
     for k in range(len(l)):
         px, py = zip(*[
             (x+k*0.05, y+k*0.05) for x in rng[0] for y in rng[1] if bd.abs(l[k][x, y]) > bd.machine_threshold()
